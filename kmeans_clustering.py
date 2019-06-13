edc12d221ca73263cb930a46b38841ec1ec2e597
Hieu Phan <huyhieu.210198@gmail.com>
2019-06-13 15:54:10 -0400

    add k-means++

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
+from scipy.spatial.distance import cdist
 
 
-def generate_random_numbers(num_count, num_cluster):
+def generate_random_numbers(num_count=1000, num_cluster=3):
     x, y = make_blobs(n_samples=num_count, n_features=2, centers=num_cluster)
     return x, y
 
@@ -11,6 +12,27 @@ def generate_random_numbers(num_count, num_cluster):
 def initialize_centroids(data, num_cluster):
     perm = np.random.permutation(data)
     centroids = perm[:num_cluster, :]
+    return centroids
+
+
+def initialize_centroids_kmpp(data, num_cluster):
+    centroids = np.zeros((num_cluster, data.shape[1]))
+    first_centroid_idx = np.random.randint(data.shape[0])
+    first_centroid = data[first_centroid_idx]
+    centroids[0, :] = first_centroid
+    distance_from_nearest_centroids = cdist(first_centroid.reshape(1, -1), data, 'euclidean').T
+    # print(distance_from_nearest_centroids)
+    centroid_count = 1
+    while centroid_count < num_cluster:
+        # Choose next centroid using weighted probability distribution
+        square_distance = np.square(distance_from_nearest_centroids)
+        weighted_distribution = square_distance / np.sum(square_distance)       # Normalize step
+        next_centroid_idx = np.random.choice(data.shape[0], p=weighted_distribution.flatten(), replace=False)
+        next_centroid = data[next_centroid_idx]
+        centroids[centroid_count - 1, :] = next_centroid
+        # Update the distance between data points and their nearest centroids
+        distance_from_nearest_centroids = np.minimum(distance_from_nearest_centroids, cdist(next_centroid.reshape(1, -1), data, 'euclidean').T)
+        centroid_count = centroid_count + 1
     return centroids
 
 
@@ -48,11 +70,13 @@ def compute_cost(data, centroid_assignment, centroid_list):
 
 
 # Run k-means. Default number of data points = 1000, default number of clusters = 3
-def run_k_mean(num_count=1000, num_cluster=3, tol=1e-7):
-    data, _ = generate_random_numbers(num_count, num_cluster)
+def run_k_mean(data, num_cluster=3, tol=1e-7, kmpp=False):
 
     # Randomly initialize centroids
-    centroids = initialize_centroids(data, num_cluster)
+    if kmpp:
+        centroids = initialize_centroids_kmpp(data, num_cluster)
+    else:
+        centroids = initialize_centroids(data, num_cluster)
     labels = assign_to_closest_centroids(data, centroids)
 
     cost_hist = []
@@ -63,7 +87,11 @@ def run_k_mean(num_count=1000, num_cluster=3, tol=1e-7):
     # Show initial assignment
     plt.scatter(data[:, 0], data[:, 1], c=labels)
     plt.scatter(centroids[:, 0], centroids[:, 1], c='r')
-    plt.title("Initial assignment")
+    plt.title("Initial assignment with %d clusters" % num_cluster)
+    if kmpp:
+        plt.savefig("Plot/Initial_kmpp")
+    else:
+        plt.savefig("Plot/Initial_vanilla")
     plt.show()
 
     while True:
@@ -79,17 +107,28 @@ def run_k_mean(num_count=1000, num_cluster=3, tol=1e-7):
     # Show final assignment
     plt.scatter(data[:, 0], data[:, 1], c=labels)
     plt.scatter(centroids[:, 0], centroids[:, 1], c='r')
-    plt.title("Final assignment")
+    plt.title("Final assignment with %d clusters" % num_cluster)
+    if kmpp:
+        plt.savefig("Plot/Final_kmpp")
+    else:
+        plt.savefig("Plot/Final_vanilla")
     plt.show()
 
     return cost_hist
 
 
 if __name__ == "__main__":
-    cost_history = run_k_mean(num_cluster=10)
+    data, _ = generate_random_numbers(num_cluster=5)
     # Plot of cost vs. iterations
-    plt.plot(cost_history)
-    plt.grid(linewidth=1.0)
-    plt.xlabel("Number of iterations")
-    plt.ylabel("Total cost")
+    cost_history = run_k_mean(data, num_cluster=5)
+    cost_history_pp = run_k_mean(data, num_cluster=5, kmpp=True)
+    fig, ax = plt.subplots(1, 2)
+    ax[0].plot(cost_history)
+    ax[0].set_title("Vanilla")
+    ax[0].set_xlabel("Number of iterations")
+    ax[0].set_ylabel("Total cost")
+    ax[1].plot(cost_history)
+    ax[1].set_title("K-means++")
+    ax[1].set_xlabel("Number of iterations")
+    ax[1].set_ylabel("Total cost")
     plt.show()
